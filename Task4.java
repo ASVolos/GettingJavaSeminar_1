baad9b13c1bd49264142f5052419b081e79f7bbd
ASVolos <146623139+ASVolos@users.noreply.github.com>
2024-02-25 20:08:53 +0500

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,16 @@
+// Задание №4 
+// Напишите метод, который находит самую длинную строку общего 
+// префикса среди массива строк.
+// Если общего префикса нет, вернуть пустую строку ""
+
+public class Task4 {
+    public static void main(String[] args) {
+        
+        String[] str = {"flower", "flow", "flight"};
+        String prefix = str[0];
+        for (int i = 1; i < str.length; i++) {
+            
+        }
+        
+    }
+}
